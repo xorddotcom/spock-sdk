b6a8a5d0591b26a5a8ffb517afcbac136d08a2ae
BilalMir135 <bilalmir135@gmail.com>
2022-08-16 19:07:31 +0500

    feat: txn events handling

--- src/constants.js
+++ src/constants.js
@@ -19,4 +19,25 @@ export const WALLET_TYPE = {
   OTHER: 'Other',
 };
 
+const ALIAS = 'dz';
+
+const withAlias = (label) => `${ALIAS}_${label}`;
+
 export const ServerEndooint = 'http://dapp-server-test.dappzero.io';
+
+export const EVENTS = {
+  SEND_TXN: withAlias('sendTxn'),
+  LEGACY_TXN_CALLBACK: withAlias('legacyTxnCallback'),
+  STORAGE_SET_ITEM: withAlias('storageSetItem'),
+  STORAGET_GET_ITEM: withAlias('storageGetItem'),
+};
+
+export const COOKIES = {
+  CACHE_ADDRESS: withAlias('cacheAddress'),
+  CACHE_CHAINID: withAlias('cacheChain'),
+};
+
+export const PROVIDER_TYPE = {
+  EIP1193: 'eip1193',
+  LEGACY: 'legacy',
+};
